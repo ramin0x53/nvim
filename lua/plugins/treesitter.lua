9c2f21c059ed5e3dd68eec3a09d37ea736b48045
ramin <ramiinnss@gmail.com>
2024-07-05 13:06:34 -0400

    improve treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "python", "lua", "go", "javascript", "html", "rust" },
+            ensure_installed = { "c", "python", "lua", "go", "javascript", "html", "rust", "cpp", "typescript", "bash", "toml", "json", "ini", "yaml" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
