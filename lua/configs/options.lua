20ec14062d170cbf6d301841f1e72b390d759a76
ramin <ramiinnss@gmail.com>
2024-07-05 14:21:05 -0400

    add some vim options

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -2,6 +2,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("xnoremap p pgvy")
+vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("set relativenumber")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
