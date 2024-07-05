6c2b14c704e5c7eac83c3867f5ca22e80d6b0b8e
ramin <ramiinnss@gmail.com>
2024-07-05 10:23:35 -0400

    set color scheme

---
+++ lua/plugins/color-scheme.lua
@@ -0,0 +1,8 @@
+return {
+    -- "nyoom-engineering/oxocarbon.nvim",
+    "bluz71/vim-moonfly-colors",
+    priority = 1000,
+    config = function()
+        vim.cmd([[colorscheme moonfly]])
+    end,
+}
