1ca539a7ac7f5fff83c289164aff063fd2eb451d
ramin <ramiinnss@gmail.com>
2024-07-11 17:22:43 -0400

    Add auto read

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -10,3 +10,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.wo.number = true
+
+
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+    command = "if mode() != 'c' | checktime | endif",
+    pattern = { "*" },
+})
